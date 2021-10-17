7ee1d021a50d73a12d41da64e37c410320989781
sixducks <82391600+sixducks@users.noreply.github.com>
2021-10-17 22:44:17 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,72 @@
+//#include<iostream>
+//
+//using namespace std;
+//int main()
+//{
+//	int a, b;
+//	cout << "a=";
+//	cin >> a;
+//	cout << "b=";
+//	cin >> b;
+//	cout << a << "+" << b << "=" << a + b << endl;
+//	cout << a << "-" << b << "=" << a - b << endl;
+//	cout << a << "*" << b << "=" << a*b << endl;
+//	cout << a << "/" << b << "=" << a / b << endl;
+//	cout << a << "%" << b << "=" << a%b << endl;
+//	return 0;
+//}
+
+//#include<iostream>
+//
+//using namespace std;
+//int main()
+//{
+//	int a = 010, b = 10, c = 0X10;
+//	cout << "DEC:";
+//	cout << "a=" << a<<' ';
+//	cout << "b=" << b<<' ';
+//	cout << "c=" << c<<endl;
+//
+//	cout << "OCT:";
+//	cout << oct;
+//	cout << "a=" << a;
+//	cout << " b=" << b<<' ';
+//	cout << "c=" << c;
+//
+//
+//	return 0;
+//}
+
+//#include<iostream>
+//
+//using namespace std;
+//int main()
+//{
+//	char c1 = 'A';
+//	char c2;
+//	c2 = c1 + 32;
+//	cout << c1 << endl;
+//	cout << "c1=" << int(c1) << endl;
+//	cout << c2 << endl;
+//	cout << "c2=" << int(c2) << endl;
+//	cout << '$' << " : " << int('$') << endl;
+//
+//	cout << "c1 c2" << endl;
+//	cout << "ÊäÈëc1 c2µÄÖµ" << endl;
+//	cin >> c1 >> c2;
+//	cout << "c1=" << c1 << " c2=" << c2 << endl;
+//	return 0;
+//}
+
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	char c1 = '\a', TAB = '\t';
+	cout << c1 << endl;
+	cout << 1 << TAB << 2 << TAB << 3 <<TAB<< 4 << endl;
+	cout << "He said \"Thank you\"." << endl;
+	cout << "abc\n" << "def" << '\n';
+	return 0;
+}
